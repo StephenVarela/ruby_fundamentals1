88e7870a06ac4d6ab8b078f98b3db6d6623ae27b
StephenVarela <varelast92@gmail.com>
2018-09-04 15:26:41 -0400

    finished exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,13 @@
 puts 2
 puts 3
 puts 2 + 3
+
+puts 'Hello World'
+puts "Hello World"
+
+puts "Stephen has been alive for #{2018-1992} years"
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
