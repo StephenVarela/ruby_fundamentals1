8e39cea3f2191ec590e559867a88eb98ad8c0327
StephenVarela <varelast92@gmail.com>
2018-09-04 15:55:30 -0400

    finished exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,11 @@
+#this is exercise of 3
+
+puts "What is your name?"
+user_input = gets.chomp
+puts "Hello #{user_input} Nice to Meet you!"
+puts "How old are you?"
+age = gets.chomp.to_i
+
+#calculate year
+year = 2018 - age
+puts "You were born in #{year}"
