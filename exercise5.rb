72165dfa61b3f427d9689610545fded9cc2de275
StephenVarela <varelast92@gmail.com>
2018-09-04 17:03:14 -0400

    added some spacing in the lines

--- exercise5.rb
+++ exercise5.rb
@@ -9,8 +9,10 @@ puts "run/walk/rest/go home"
 puts "try not to die!"
 
 while(true)
+
   puts "Would you like to walk or run?"
   user_action = gets.chomp.upcase
+
   if(user_action == "WALK")
     if(energy < 100)
       energy += 1
@@ -33,15 +35,17 @@ while(true)
   else
     puts "Error! Invalid command"
   end
+
   if(energy < 0)
     puts "Oh dear... you died"
     break
   end
+
   puts "Distance from home is #{distance}Km."
   puts "Your Energy is at #{energy}\%."
+
   if (energy < 20)
     puts "Warning your energy is low! Take a break"
   end
-
 
 end
